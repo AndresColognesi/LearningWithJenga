05490f012ba1a2040801d913776542e3d0ae590b
AndresColognesi <a.cologne9@gmail.com>
2023-06-05 22:31:27 -0300

    style: Delete unused code from API_Request script

--- Assets/Scripts/API_Request.cs
+++ Assets/Scripts/API_Request.cs
@@ -25,44 +25,6 @@ public class API_Request : MonoBehaviour
 
     #region Custom Methods
 
-    private JengaBlockData TESTParseData(string request_text)
-    {
-        /***
-         * Receives a request text in JSON format and properly stores it
-         * in a list of objects, returning it.
-         ***/
-        Debug.Log(request_text);
-        // Create object to store data:
-        JengaBlockData data = JsonUtility.FromJson<JengaBlockData>(request_text);
-        return data;
-
-    }
-
-    private List<JengaBlockData> TESTParseDataLIST(string request_text)
-    {
-        /***
-         * Receives a request text in JSON format and properly stores it
-         * in a list of objects, returning it.
-         ***/
-        //Debug.Log(request_text);
-        List<string> data = new List<string>();
-        foreach (Match match in Regex.Matches(request_text, @"(\{.+""\})(,|\])"))
-        {
-            Debug.Log(match.Value);
-            data.Add(match.Value);
-        }
-        //string[] result = Regex.Matches(request_text, @"\{.+\},?").Cast<Match>().Select(m => m.Value).ToArray();
-        // Create object to store data:
-        List<JengaBlockData> objectData = new List<JengaBlockData>();
-        foreach (string entry in data)
-        {
-            objectData.Add(JsonUtility.FromJson<JengaBlockData>(entry));
-        }
-        
-        return objectData;
-
-    }
-
     private JengaBlockDataListObject ParseData(string request_text)
     {
         /***
@@ -136,25 +98,6 @@ public class API_Request : MonoBehaviour
         }
     }
 
-    public List<JengaBlockData> GetTowerPiecesList(string towerName)
-    {
-        /***
-         * Retrieve the list of Jenga Blocks for the given tower. The
-         * name of the tower follows the received payload format.
-         ***/
-        switch (towerName)
-        {
-            case "6th Grade":
-                return Pieces6thGradeList;
-            case "7th Grade":
-                return Pieces7thGradeList;
-            case "8th Grade":
-                return Pieces8thGradeList;
-            default: // return empty list
-                return new List<JengaBlockData>();
-        }
-    }
-
     #endregion
 
 
@@ -165,32 +108,6 @@ public class API_Request : MonoBehaviour
     {
         // Execute get request on desired API:
         StartCoroutine(GetRequest(url));
-        //string test_data = "{\"id\": 2,\"subject\": \"Math\",\"grade\": \"6th Grade\",\"mastery\": 2,\"domainid\": \"RP\",\"domain\": \"Ratios & Proportional Relationships\",\"cluster\": \"Understand ratio concepts and use ratio reasoning to solve problems.\",\"standardid\": \"CCSS.MATH.CONTENT.6.RP.A.1\",\"standarddescription\": \"oi\"}";
-        //JengaBlockData data = TESTParseData(test_data);
-        //Debug.Log(data.standarddescription);
-        //string test_data_full = "{\"listOfData\": [{\"id\": 2,\"subject\": \"Math\",\"grade\": \"6th Grade\",\"mastery\": 2,\"domainid\": \"RP\",\"domain\": \"Ratios & Proportional Relationships\",\"cluster\": \"Understand ratio concepts and use ratio reasoning to solve problems.\",\"standardid\": \"CCSS.MATH.CONTENT.6.RP.A.1\",\"standarddescription\": \"oi\"}]}";
-        //AllData dataList = ParseData(test_data_full);
-        //Debug.Log(dataList.listOfdata);
-
-
-        //string test_dataList_full = "[{\"id\": 2,\"subject\": \"Math\",\"grade\": \"6th Grade\",\"mastery\": 2,\"domainid\": \"RP\",\"domain\": \"Ratios & Proportional Relationships\",\"cluster\": \"Understand ratio concepts and use ratio reasoning to solve problems.\",\"standardid\": \"CCSS.MATH.CONTENT.6.RP.A.1\",\"standarddescription\": \"oi\"},{\"id\": 5,\"subject\": \"Math\",\"grade\": \"6th Grade\",\"mastery\": 2,\"domainid\": \"RP\",\"domain\": \"Ratios & Proportional Relationships\",\"cluster\": \"Understand ratio concepts and use ratio reasoning to solve problems.\",\"standardid\": \"CCSS.MATH.CONTENT.6.RP.A.1\",\"standarddescription\": \"tchau\"}]";
-        //dataList = TESTParseDataLIST(test_dataList_full);
-        
-
-        // CREATE TEST DATA LIST TO FINISH PROJECT:
-        /*dataList = new List<JengaBlockData>();
-        string test_data = "{\"id\": 2,\"subject\": \"Math\",\"grade\": \"6th Grade\",\"mastery\": 2,\"domainid\": \"RP\",\"domain\": \"Ratios & Proportional Relationships\",\"cluster\": \"Understand ratio concepts and use ratio reasoning to solve problems.\",\"standardid\": \"CCSS.MATH.CONTENT.6.RP.A.1\",\"standarddescription\": \"oi\"}";
-        JengaBlockData data = TESTParseData(test_data);
-        dataList.Add(data);
-        string test_data2 = "{\"id\": 5,\"subject\": \"Math\",\"grade\": \"6th Grade\",\"mastery\": 2,\"domainid\": \"RP\",\"domain\": \"Ratios & Proportional Relationships\",\"cluster\": \"Understand ratio concepts and use ratio reasoning to solve problems.\",\"standardid\": \"CCSS.MATH.CONTENT.6.RP.A.1\",\"standarddescription\": \"tchau\"}";
-        JengaBlockData data2 = TESTParseData(test_data2);
-        dataList.Add(data2);
-        dataList.Add(data2);
-        dataList.Add(data2);
-        dataList.Add(data2);
-        dataList.Add(data2);
-        dataList.Add(data2);
-        dataList.Add(data2);*/
 
     }
 
