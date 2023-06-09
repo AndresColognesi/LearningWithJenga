57a149c7c5d9c1c7136aea058baad31ab3019dee
AndresColognesi <a.cologne9@gmail.com>
2023-06-09 16:05:07 -0300

    feat: WIP Create mouse camera movement functionality

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -8,9 +8,26 @@ public class CameraManager : MonoBehaviour
 
     // Array of transforms for desired camera anchors:
     [SerializeField] private Transform[] cameraAnchorArray;
+    // Array of all stack transforms:
+    [SerializeField] private Transform[] stackTransformArray;
+
+    // Mouse Camera Movement Parameters //
+    // Mouse sensitivity to hover:
+    [SerializeField] private float mouseSensitivity = 3.0f;
+    // Current rotations:
+    private float rotationY;
+    private float rotationX;
+    private Vector3 currentRotation;
+    // Rotation smoothness parameters:
+    private Vector3 smoothVelocity = Vector3.zero;
+    [SerializeField] private float smoothTime = 0.2f;
+    // Rotation limits in Euler Angles:
+    [SerializeField] private Vector2 rotationXMinMax = new Vector2(-30, 30);
 
     // Store current stack on focus:
     private int currentStackIndex;
+    // Store current stack radius (camera - stack distance):
+    private float currentRadius;
 
     #endregion
 
@@ -41,8 +58,9 @@ public class CameraManager : MonoBehaviour
         // Set position and rotation of camera:
         gameObject.transform.position = cameraAnchorArray[desired_index].position;
         gameObject.transform.rotation = cameraAnchorArray[desired_index].rotation;
-        // Store current stack that is focused on:
+        // Store current stack that is focused on and radius:
         currentStackIndex = desired_index;
+        currentRadius = Mathf.Abs((cameraAnchorArray[desired_index].position - stackTransformArray[desired_index].position).magnitude);
     }
 
     public int GetCurrentStackIndex()
@@ -52,6 +70,31 @@ public class CameraManager : MonoBehaviour
          ***/
 
         return currentStackIndex;
+    }
+
+    private void MouseCameraRotation()
+    {
+        /***
+         * Rotate the camera (this game object) around the desired
+         * tower/stack.
+         ***/
+
+        // Get mouse left click input:
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        // Update current axis rotations:
+        rotationY += mouseX;
+        rotationX += mouseY;
+        // Clamp X rotation: 
+        rotationX = Mathf.Clamp(rotationX, rotationXMinMax.x, rotationXMinMax.y);
+        // Calculate next rotation abd apply damping:
+        Vector3 nextRotation = new Vector3(rotationX, rotationY);
+        currentRotation = Vector3.SmoothDamp(currentRotation, nextRotation, ref smoothVelocity, smoothTime);
+        // Apply final desired rotation:
+        transform.localEulerAngles = currentRotation;
+
+        // Point camera to stack and place in defined distance:
+        transform.position = stackTransformArray[currentStackIndex].position - transform.forward * currentRadius;
     }
 
     #endregion
@@ -64,6 +107,11 @@ public class CameraManager : MonoBehaviour
     {
         // Set camera to focus a default tower:
         SetCameraToAnchor("7th Grade");
+    }
+
+    void Update()
+    {
+        MouseCameraRotation();
     }
 
     #endregion
